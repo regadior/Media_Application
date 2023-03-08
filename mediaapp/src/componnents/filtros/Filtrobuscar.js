1655bd623a454505b57113f2f96fef3878efe36f
regadior <regadior@gmail.com>
2023-03-08 15:57:42 +0100

    Inicio Filtro de buscar v1

--- mediaapp/src/componnents/filtros/Filtrobuscar.js
+++ mediaapp/src/componnents/filtros/Filtrobuscar.js
@@ -1,15 +1,31 @@
-import React from 'react'
-import lupa from '../icons/lupa.png'
-import './Filtro.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import lupa from '../icons/lupa.png';
+import './Filtro.css';
+const RAWG_API_KEY=proces.env.RAWG_API_KEY;
+const RAWG_BASE_URL=proces.env.RAWG_BASE_URL;
 function Filtrobuscar() {
-  return (
-    <div class="Filtrobuscar">
-      <input type='text' placeholder='Buscar' className='Filtrobuscar_input'></input>
-      <button type='submit' className='Filtrobuscar_boton'>
-        <img src={lupa}></img>
-      </button>
-   </div>
-  )
+    const [inputSearch, setinputSearch] = useState("");
+    const [resultadoJuego, setresultadoJuego] = useState([]);
+    const handleChange = (e) => {
+        setinputSearch(e.target.value);
+        console.log("Estas escribiendo", e.target.value);
+    }
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        useEffect(() =>{
+            axios.get()
+        })
+    }
+
+    return (
+        <div class="Filtrobuscar">
+            <input type='text' placeholder='Buscar' className='Filtrobuscar_input' value={inputSearch} onChange={handleChange}></input>
+            <button type='submit' className='Filtrobuscar_boton' onSubmit={onSubmit}>
+                <img src={lupa}></img>
+            </button>
+        </div>
+    )
 }
 
 export default Filtrobuscar;
